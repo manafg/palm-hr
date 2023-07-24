1439aa9f3c2281c3e9712258a8381720decc4d52
manafG <manafhgh22@gmail.com>
2023-07-25 01:51:31 +0300

    wrap the providers

---
+++ src/main/context/index.ts
@@ -0,0 +1,10 @@
+import {BooksInfoProvider, BooksInfoStateContext} from './BooksContext/BooksContext';
+import QueryContextProvider from './QueryContext/QueryContextProvider';
+import PalmThemeProvider from './ThemeProvider/PalmThemeProvider';
+
+export {
+    BooksInfoProvider,
+    BooksInfoStateContext,
+    QueryContextProvider,
+    PalmThemeProvider
+}
